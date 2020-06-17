e6734c564af64e5543e05346462a80ad521ccc43
Keith Franklin <keith30895@gmail.com>
2020-06-17 15:37:20 +0530

    bringing back public

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import Home from './home/home';
+// import Home from './home/home';
 class Main extends Component {
     render() {
         return (
-            <div>
-                <Route exact path='/' render={(props)=>{
-                    return (<Home/>);
-                }}/>
-                <Route path='/project' render={(props) => {
-                    return (<h1>Project</h1>);
-                }} />
-            </div>
+            <Router>
+                <Switch>
+                    <div>
+                        <Route exact path='/' render={(props) => {
+                            return (<h1>Home</h1>);
+                        }} />
+                        <Route path='/project' render={(props) => {
+                            return (<h1>Project</h1>);
+                        }} />
+                    </div>
+                </Switch>
+            </Router>
         );
     }
 }
