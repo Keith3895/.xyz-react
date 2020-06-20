6342fdfbf21e6c02825f0fcf48350451a184d2b7
Keith Franklin <keith30895@gmail.com>
2020-06-20 10:36:08 +0530

    project page completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import Home from './home/home';
-// import Home from './home/home';
+import ProjectsScroll from './projectsPage/projectScroll';
 class Main extends Component {
     render() {
         return (
@@ -11,8 +11,19 @@ class Main extends Component {
                     return (<Home />);
                 }} />
                 <Route path='/project' render={(props) => {
-                    return (<h1>Project</h1>);
+                    return (
+                        <div>
+                            <h1>Project</h1>
+                            <ProjectsScroll />
+                        </div>
+                    );
                 }} />
+                <Route path='/blog' render={(props) => {
+                        window.open('http://blog.keithfranklin.xyz/');
+                        return(<h5>
+                        redirecting to blog
+                    </h5>);
+                }}/>
             </Switch>
             // </Router>
         );
