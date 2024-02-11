d5574fb753fc7b2912924cc89b8962a1f9e57477
Keith <keith30895@gmail.com>
2024-02-11 16:34:08 +0530

    Add NavBar component

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,8 @@ import Home from "./pages/home";
 import BlogList from "./pages/blog-list";
 import Footer from "./components/footer";
 import Blog, { loader as BlogLoader } from "./pages/blog";
+import NavBar from "./components/nav";
 export default class Main extends Component {
-    navigation = [
-        { name: 'About me', href: '/' },
-        { name: 'Blogs', href: '#/blogs' },
-        { name: 'Projects', href: '#' },
-    ];
 
     router = createHashRouter([
         {
@@ -30,29 +26,9 @@ export default class Main extends Component {
     ]);
     render() {
         return (
-
-
-            // navBar
-
             <div>
                 <header >
-                    <nav className="items-right lg:px-8 bg-black text-white p-4 flex justify-between fixed w-full top-0 z-50 shadow-md"
-                        aria-label="Global">
-                        <div className="space-x-4">
-
-                        </div>
-
-                        <div className="hidden lg:flex lg:gap-x-16">
-                            {this.navigation.map((item) => (
-                                <a key={item.name} href={item.href} className="flex items-center text-sm text-center font-bold leading-6 ">
-                                    {item.name}
-                                </a>
-                            ))}
-                            <a href="mailto:keith30895@gmail.com" className="w-36 h-10 bg-custom-orange-600 rounded-3xl justify-center items-center gap-2.5 inline-flex ">
-                                <div className="text-center text-black text-base font-bold font-['Raleway']">CONTACT ME</div>
-                            </a>
-                        </div>
-                    </nav>
+                    <NavBar />    
                 </header>
                 <div className="mt-24 md:mt-16 pt-2">
                     <RouterProvider router={this.router} />
